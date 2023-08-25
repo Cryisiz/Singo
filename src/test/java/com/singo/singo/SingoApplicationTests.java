afc1688c2e8fabafdbdaed568423c6db06610df0
Cheong Zhe Min <Cheongzhemin@gmail.com>
2023-08-25 23:07:00 +0800

    first commit

---
+++ src/test/java/com/singo/singo/SingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singo.singo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
